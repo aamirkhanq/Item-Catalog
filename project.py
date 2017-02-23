a6fcae06ce38d6e50096c08f90a0a4c6e784cb18
Aamir Khan <aamir.k56@gmail.com>
2017-02-23 23:19:52 +0530

    Added project.py: Main file that contains all server side code

---
+++ project.py
@@ -0,0 +1,20 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/index')
+def mainpage():
+    return 'You are at the main page.'
+
+@app.route('/index/<int:category_id>/')
+def showCategoryItems(category_id):
+    return 'You are on categories item page.'
+
+@app.route('/index/<int:category_id>/<int:item_id>')
+def showItem():
+    return 'You are on the item page.'
+
+if __name__ == '__main__':
+    app.secret_key = 'super_secret_key'
+    app.debug = True
+    app.run(host='0.0.0.0', port=80)
