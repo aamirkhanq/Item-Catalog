813b551e84a7e36d6de1264ba6c86baf6f8205ba
Aamir Khan <aamir.k56@gmail.com>
2017-03-01 02:21:02 +0530

    Make PEP Compliant

--- database_setup.py
+++ database_setup.py
@@ -1,4 +1,6 @@
-import sys, datetime
+import sys
+
+import datetime
 
 from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
 
@@ -10,46 +12,47 @@ from sqlalchemy import create_engine
 
 Base = declarative_base()
 
+
 class User(Base):
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
     email = Column(String(250), nullable=False)
-    
+
+
 class Category(Base):
     __tablename__ = 'categories'
-    name = Column(String(80), nullable = False)
-    id = Column(Integer, primary_key = True)
-    
+    name = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
+
     @property
     def serialize(self):
-        #Returns object data in easily serializable format
+        # Returns object data in easily serializable format
         return {
-            'id' : self.id,
-            'name' : self.name,
+            'id': self.id,
+            'name': self.name,
             }
+
 
 class Item(Base):
     __tablename__ = 'items'
-    name = Column(String(80), nullable = False)
-    id = Column(Integer, primary_key = True)
+    name = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
     description = Column(String(250))
     category_id = Column(Integer, ForeignKey('categories.id'))
     categories = relationship(Category)
-    #created_date = Column(DateTime, default = datetime.datetime.utcnow)
     user_id = Column(Integer, ForeignKey('users.id'))
     users = relationship(User)
 
     @property
     def serialize(self):
-        #Returns object data in easily serializable format
+        # Returns object data in easily serializable format
         return {
-            'id' : self.id,
-            'name' : self.name,
-            'description' : self.description,
+            'id': self.id,
+            'name': self.name,
+            'description': self.description,
             }
 
 engine = create_engine('sqlite:///itemcatalog.db')
 Base.metadata.create_all(engine)
-
